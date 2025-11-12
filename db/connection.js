ac310f0c378ab959faa7fb7cbf3842032f3eac54
jayrold <127464544+blackhatsV1@users.noreply.github.com>
2025-11-13 05:35:02 +0800

    Refactor database connection settings
    Updated database connection configuration to use environment variables and added SSL options.

--- db/connection.js
+++ db/connection.js
@@ -4,10 +4,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST||'localhost',
-  user: process.env.DB_USER||'root',
-  password: process.env.DB_PASSWORD||'',
-  database: process.env.DB_NAME||'getsetkiddies',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: true }, 
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 db.connect((err) => {
